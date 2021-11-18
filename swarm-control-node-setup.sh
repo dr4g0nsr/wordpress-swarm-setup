ab1950d2351b5e5e7d5f0356b60eb7afe7fe3c11
Dragutin Cirkovic <dragonmen@gmail.com>
2021-11-18 19:44:27 +0100

    Update swarm-control-node-setup.sh

--- swarm-control-node-setup.sh
+++ swarm-control-node-setup.sh
@@ -5,7 +5,11 @@ apt install -y nfs-server nfs-common docker.io
 docker swarm init
 apt update
 
+mkdir /nfs
 mkdir /nfs/{db,wordpress}
+chown nobody:nogroup /nfs
+chown nobody:nogroup /nfs/* -R
+
 cp exports /etc/exports
 
 systemctl nfs-server restart
